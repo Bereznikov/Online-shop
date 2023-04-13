92110a1b5f1e6a4e72cd64e0e29ee1ef684d99c3
Bereznikov <aleksey.bereznikov@gmail.com>
2023-04-13 18:48:43 +0300

    Users app added

--- store/store/urls.py
+++ store/store/urls.py
@@ -18,12 +18,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import include, path
 
-from products.views import index, products
+from products.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users'))
+
 ]
 
 if settings.DEBUG:

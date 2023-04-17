9ca4dd9f747d45ed538f2644e9c3730c84580e1e
Bereznikov <aleksey.bereznikov@gmail.com>
2023-04-18 02:51:29 +0300

    User views rebased on CBV User profile access check added

--- store/users/views.py
+++ store/users/views.py
@@ -1,68 +1,51 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.db.models import Sum
-from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
 
+from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm
-    context = {'form': form}
-    return render(request, 'users/login.html', context)
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
+
+    def get_context_data(self, **kwargs):
+        context = super(UserRegistrationView, self).get_context_data()
+        context['title'] = 'Store - Регистрация'
+        return context
+
+    def form_valid(self, form):
+        responce = super().form_valid(form)
+        messages.success(self.request, 'Вы успещно зарегестрировались!')
+        return responce
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Регистрация прошла успешно")
-            return HttpResponseRedirect(reverse('users:login'))
-    else:
-        form = UserRegistrationForm()
-    context = {'form': form}
-    return render(request, 'users/registration.html', context)
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
 
 
-@login_required()
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('users:profile'))
-        else:
-            print(form.errors)
-    else:
-        form = UserProfileForm(instance=request.user)
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
 
-    baskets = Basket.objects.filter(user=request.user)
-    total_sum = sum(basket.sum() for basket in baskets)
-    total_quantity = baskets.aggregate(Sum('quantity'))
+    def get_success_url(self):
+        return reverse_lazy('users/profile.html', args=(self.object.id))
 
-    context = {
-        'title': 'Личный кабинет',
-        'form': form,
-        'baskets': Basket.objects.filter(user=request.user),
-        'total_sum': total_sum,
-        'total_quantity': total_quantity['quantity__sum'],
-    }
-    return render(request, 'users/profile.html', context)
+    def get_context_data(self, **kwargs):
+        baskets = Basket.objects.filter(user=self.request.user)
 
-
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
+        context = super(UserProfileView, self).get_context_data()
+        context['title'] = 'Store - Личный кабинет'
+        context['baskets'] = baskets
+        context['total_sum'] = sum(basket.sum() for basket in baskets)
+        context['total_quantity'] = baskets.aggregate(Sum('quantity')).get('quantity__sum')
+        return context

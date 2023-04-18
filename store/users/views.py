743b75684919073a09a9cd8e35cb0466a859c709
Bereznikov <aleksey.bereznikov@gmail.com>
2023-04-18 03:46:35 +0300

    bag fixed TitleMixin added

--- store/users/views.py
+++ store/users/views.py
@@ -1,30 +1,23 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth, messages
-from django.urls import reverse, reverse_lazy
+from django.contrib import messages
+from django.urls import reverse_lazy
 from django.db.models import Sum
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
+from common.views import TitleMixin
 
 
-class UserRegistrationView(CreateView):
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = 'users/registration.html'
     success_url = reverse_lazy('users:login')
-
-    def get_context_data(self, **kwargs):
-        context = super(UserRegistrationView, self).get_context_data()
-        context['title'] = 'Store - Регистрация'
-        return context
-
-    def form_valid(self, form):
-        responce = super().form_valid(form)
-        messages.success(self.request, 'Вы успещно зарегестрировались!')
-        return responce
+    success_message = 'Вы успещно зарегестрировались!'
+    title = 'Store - Регистрация'
 
 
 class UserLoginView(LoginView):
@@ -32,19 +25,19 @@ class UserLoginView(LoginView):
     form_class = UserLoginForm
 
 
-class UserProfileView(UpdateView):
+class UserProfileView(TitleMixin, UpdateView):
     model = User
     form_class = UserProfileForm
     template_name = 'users/profile.html'
+    title = 'Store - Личный кабинет'
 
     def get_success_url(self):
-        return reverse_lazy('users/profile.html', args=(self.object.id))
+        return reverse_lazy('users:profile', args=(self.object.id,))
 
     def get_context_data(self, **kwargs):
         baskets = Basket.objects.filter(user=self.request.user)
 
         context = super(UserProfileView, self).get_context_data()
-        context['title'] = 'Store - Личный кабинет'
         context['baskets'] = baskets
         context['total_sum'] = sum(basket.sum() for basket in baskets)
         context['total_quantity'] = baskets.aggregate(Sum('quantity')).get('quantity__sum')

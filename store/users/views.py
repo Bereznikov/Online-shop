c48a72e27cfbbe25144e52320402478a052e143c
Bereznikov <aleksey.bereznikov@gmail.com>
2023-04-19 04:20:47 +0300

    flake8 & isort added Pep8 fix

--- store/users/views.py
+++ store/users/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import HttpResponseRedirect
-from django.urls import reverse_lazy, reverse
-from django.db.models import Sum
-from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView
 
-from users.models import User
-from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm, EmailVerification
-from products.models import Basket
 from common.views import TitleMixin
+from users.forms import (EmailVerification, UserLoginForm, UserProfileForm,
+                         UserRegistrationForm)
+from users.models import User
 
 
 class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):

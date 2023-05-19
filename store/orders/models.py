c4d966335fbc045e53244518d99706382f74cdb0
Bereznikov <aleksey.bereznikov@gmail.com>
2023-05-20 02:12:35 +0300

    Hardest bug ever fixed

--- store/orders/models.py
+++ store/orders/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 from users.models import User
+from products.models import Basket
+
 
 class Order(models.Model):
     CREATED = 0
     PAID = 1
     ON_WAY = 2
-    DELIVERED =3
+    DELIVERED = 3
     STATUSES = (
         (CREATED, 'Создан'),
         (PAID, 'Оплачен'),
@@ -26,4 +28,12 @@ class Order(models.Model):
     def __str__(self):
         return f'Заказ №{self.id}. {self.first_name} {self.last_name}'
 
-
+    def update_after_payment(self):
+        baskets = Basket.objects.filter(user=self.initiator)
+        self.status = self.PAID
+        self.basket_history = {
+            'purchased_items': [basket.de_json() for basket in baskets],
+            'total_sum': float(sum([basket.sum() for basket in baskets])),
+        }
+        baskets.delete()
+        self.save()
--- store/orders/views.py
+++ store/orders/views.py
@@ -9,12 +9,15 @@ from django.http import HttpResponse
 from common.views import TitleMixin
 from orders.forms import OrderForm
 from products.models import Basket
+from orders.models import Order
 
 from http import HTTPStatus
 
 import stripe
+import json
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
+endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
 
 
 class SuccessTemplateView(TitleMixin, TemplateView):
@@ -57,34 +60,62 @@ class OrderCreateView(TitleMixin, CreateView):
         return super(OrderCreateView, self).form_valid(form)
 
 
+# @csrf_exempt
+# def stripe_webhook_view(request):
+#     payload = request.body.decode('utf-8')
+#     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+#     event = None
+#
+#     try:
+#         event = stripe.Webhook.construct_event(
+#             payload, sig_header, settings.STRIPE_WEBHOOK_SECRET,
+#         )
+#     except ValueError as e:
+#         # Invalid payload
+#         return HttpResponse(status=400)
+#     except stripe.error.SignatureVerificationError as e:
+#         # Invalid signature
+#         return HttpResponse(status=400)
 @csrf_exempt
 def stripe_webhook_view(request):
-    payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
+    payload = request.body.decode('utf-8')
 
     try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
-        )
-    except ValueError:
-        # Invalid payload
+        event = json.loads(payload)
+    except:
+        print('⚠️  Webhook error while parsing basic request.' + str(e))
         return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError:
-        # Invalid signature
-        return HttpResponse(status=400)
+    if endpoint_secret:
+        # Only verify the event if there is an endpoint secret defined
+        # Otherwise use the basic event deserialized with json
+        sig_header = request.headers.get('stripe-signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, endpoint_secret
+            )
+        except stripe.error.SignatureVerificationError as e:
+            print('⚠️  Webhook signature verification failed.' + str(e))
+            return HttpResponse(status=400)
 
     # Handle the checkout.session.completed event
     if event['type'] == 'checkout.session.completed':
-        session = event['data']['object']
+        # Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+        session = stripe.checkout.Session.retrieve(
+            event['data']['object']['id'],
+        )
 
+        metadata = session.metadata
+        # line_items = session.line_items  -- в доке stripe
         # Fulfill the purchase...
-        fulfill_order(session)
+        fulfill_order(metadata)
 
     # Passed signature verification
     return HttpResponse(status=200)
 
 
-def fulfill_order(session):
-    order_id = int(session.metadata.order_id)
+def fulfill_order(metadata):
+    order_id = int(metadata.order_id)
+    order = Order.objects.get(id=order_id)
+    order.update_after_payment()
     print("Fulfilling order")

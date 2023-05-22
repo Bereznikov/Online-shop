fce447b1c5da484e51ea496d8d70fa8a678423cb
Bereznikov <aleksey.bereznikov@gmail.com>
2023-05-22 22:49:30 +0300

    env and requirements added

--- store/products/views.py
+++ store/products/views.py
@@ -28,7 +28,7 @@ class ProductsListView(TitleMixin, ListView):
         elif section_id:
             return queryset.filter(section_id=section_id)
         else:
-            return queryset
+            return queryset.order_by('section_id')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         section_id = self.kwargs.get('section_id')

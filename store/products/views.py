552a75dfe72732f2379cdd50e28c28df336b8a33
Bereznikov <aleksey.bereznikov@gmail.com>
2023-05-26 08:04:52 +0300

    bug fixed

--- store/products/views.py
+++ store/products/views.py
@@ -33,7 +33,7 @@ class ProductsListView(TitleMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         category_id = self.kwargs.get('category_id')
         if category_id:
-            section_id = ProductCategory.objects.get(category_id=category_id).section
+            section_id = ProductCategory.objects.get(id=category_id).section_id
         else:
             section_id = self.kwargs.get('section_id')
         context = super(ProductsListView, self).get_context_data()
